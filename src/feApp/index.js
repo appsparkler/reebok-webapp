963e20ca67d71173e69ecea7621f124404f0d0e1
Akash <subs@appsparkler.com>
2019-06-05 15:35:16 +0530

    WIP

--- src/feApp/index.js
+++ src/feApp/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap\\dist\\css\\bootstrap.css'
 
 import 'experiences/global/container'
+import 'components/global/navbar'
 import 'components/global/bootstrapCarousel'
 
 import 'templates/global/BasePage/index.js'
