5aef517343103ac5b7480015043d2c21c2ae569c
Akash <subs@appsparkler.com>
2019-06-05 15:13:08 +0530

    WIP

--- src/templates/global/BasePage/index.js
+++ src/templates/global/BasePage/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap'
 import 'bootstrap\\dist\\css\\bootstrap.css'
-import './styles.scss'
+
 import { initialize_VueComponents, initialize_VueApps } from 'common-script'
 
 initialize_VueComponents();
