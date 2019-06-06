3bc3ec6ec9993e64e9bba98d679f994370fd39da
Akash <subs@appsparkler.com>
2019-06-06 12:23:38 +0530

    WIP

--- src/feApp/index.js
+++ src/feApp/index.js
@@ -2,7 +2,8 @@ import 'bootstrap\\dist\\css\\bootstrap.css'
 
 // import 'components/bootstrap/card'
 // import 'experiences/global/container'
-import 'experiences/global/2Column'
+// import 'experiences/global/2Column'
+import 'components/bootstrap/button'
 // import 'components/global/navbar'
 // import 'components/global/bootstrapCarousel'
 
