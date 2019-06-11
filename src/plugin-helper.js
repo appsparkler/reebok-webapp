3acbd5df2441dc9a7e267990328a3ec072df82f9
Akash <subs@appsparkler.com>
2019-06-11 11:38:33 +0530

    WIP

--- src/plugin-helper.js
+++ src/plugin-helper.js
@@ -1,32 +1,30 @@
 // import {remove_allAtributesFromHTMLOtherThanIs} from 'common-script'
 export default class VueComponentNameAndTemplateExtractor {
   constructor(el) {
+    const attributesToRemove = [":component-api"]
     this.clonedEl = el.cloneNode(true);
-    const nestedIS = this.clonedEl.querySelectorAll('[is] [is]');
-    nestedIS.forEach(remove_allAttributesFromHTMLOtherThanIs)
-    nestedIS.forEach(elem => console.log(elem.outerHTML))
+    const nestedComponents = this.clonedEl.querySelectorAll('[is] [is] [is]');
+    nestedComponents
+      .forEach(
+        remove_blockingAttributes
+          .bind(null, attributesToRemove)
+      );
+    remove_blockingAttributes.bind(null, attributesToRemove, this.clonedEl);
     // const $clone = $(el).clone();
     // removing attributes other than "is" from 2nd Gen elements.
     // $clone.find('[is] [is]').each(remove_allAtributesFromHTMLOtherThanIs);
     // const clonedEl = $clone.get(0);
     // $clone.removeAttr(':component-api');
-    // this.name = clonedEl.attributes.is.value;
-    // clonedEl.attributes.removeNamedItem('is');
-    // this.template = clonedEl.outerHTML.replace(/is=".*?"/, '');
+    // this.clonedEl.removeAttribute(':component-api');
+    this.name = this.clonedEl.attributes.is.value.toString();
+    this.clonedEl.removeAttribute('is');
+    this.template = this.clonedEl.outerHTML.replace(/is=".*?"/, '');
+    console.log(this.template);
   }
 }
 
-function remove_allAttributesFromHTMLOtherThanIs(el) {
-  console.log(el)
-  Object.keys(el.attributes).reverse().forEach(delete_NonIsAttribute.bind(el))
-}
-
-function delete_NonIsAttribute(key, value, attributeArray) {
-  try {
-    const namedItem = this.attributes.item(key);
-    const name = namedItem.name;
-    if(name == ':component-api') this.attributes.removeNamedItem(name);
-  } catch (e) {
-    console.error(e);
-  }
+function remove_blockingAttributes(attributesToRemove, el) {
+    attributesToRemove.forEach(attr => el.removeAttribute(attr))
+    // el.removeAttribute(":component-api");
+    // Object.keys(el.attributes).reverse().forEach(delete_NonIsAttribute.bind(el))
 }
