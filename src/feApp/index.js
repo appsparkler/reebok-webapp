6b7ae2d14d3e44e53a11aad5a83ae5a124347f48
Akash <subs@appsparkler.com>
2019-06-11 09:29:24 +0530

    WIP

--- src/feApp/index.js
+++ src/feApp/index.js
@@ -1,15 +1,14 @@
-import 'bootstrap'
-import 'bootstrap\\dist\\css\\bootstrap.css'
-
-
-import 'components/bootstrap/card'
-import 'components/bootstrap/button'
-
-import 'components/global/navbar'
-import 'components/global/bootstrapCarousel'
+// import 'bootstrap'
+// import 'bootstrap\\dist\\css\\bootstrap.css'
 
 import 'experiences/global/container'
-import 'experiences/global/2Column'
-import 'experiences/global/sixCellLayout'
+// import 'experiences/global/2Column'
+// import 'experiences/global/sixCellLayout'
+
+import 'components/global/navbar'
+import 'components/bootstrap/button'
+// import 'components/bootstrap/card'
+// import 'components/global/bootstrapCarousel'
+
 
 import 'templates/global/BasePage/index.js'
