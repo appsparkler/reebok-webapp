9462c7274b93925ddd60d7fcc04f482c7f1b5c5b
Akash <subs@appsparkler.com>
2019-06-11 16:48:56 +0530

    WIP

--- src/feApp/index.js
+++ src/feApp/index.js
@@ -2,7 +2,7 @@
 // import 'bootstrap\\dist\\css\\bootstrap.css'
 
 // import 'experiences/global/container'
-// import 'experiences/global/2Column'
+import 'experiences/global/2Column'
 // import 'experiences/global/sixCellLayout'
 
 // import 'components/global/navbar'
