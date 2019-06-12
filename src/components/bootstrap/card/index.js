3e08c4b8c1be776d5e41ea159fefcf5f6bb64b17
Akash <subs@appsparkler.com>
2019-06-12 13:08:06 +0530

    WIP

--- src/components/bootstrap/card/index.js
+++ src/components/bootstrap/card/index.js
@@ -2,18 +2,16 @@ import $ from 'jquery';
 import Vue from 'vue';
 //
 import BootstrapCardComponentConfig from './BootstrapCardConfig'
-global.vueComponentsConfig = global.vueComponentsConfig || [];
+global.vueComponentConfigs = global.vueComponentConfigs || [];
 setup_cardComponents();
 
 // private functions
 function setup_cardComponents() {
-    console.log($('[is^=xp-bootstrap-card]').length)
     $('[is^=xp-bootstrap-card]').each(generate_cardComponent);
 }
 
 function generate_cardComponent() {
-    alert(typeof this);
-    global.vueComponentsConfig.push({
+    global.vueComponentConfigs.push({
       config: new BootstrapCardComponentConfig(this),
       el: this
     });
