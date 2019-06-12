876016a85415139bf9f9cb265fc0446e4958f54f
Akash <subs@appsparkler.com>
2019-06-12 13:39:00 +0530

    WIP

--- src/experiences/global/2Column/Global_2ColumnConfig.js
+++ src/experiences/global/2Column/Global_2ColumnConfig.js
@@ -4,12 +4,12 @@ import './styles.scss';
 // scripts
 export default class {
   config = {
-      data() {
-        return {
-          ReebokButtonConfig: {
-            buttonClicked() {
-              alert('This is 2 Column Experience.')
-            }
+    data() {
+      return {
+        ReebokButtonConfig: {
+          buttonClicked() {
+            alert('This is 2 Column Experience.')
+          }
         }
       }
     }
