a52e1c99bfc0fce997fcd5da748782b70ba8f59e
Akash <subs@appsparkler.com>
2019-06-13 15:00:01 +0530

    WIP

--- src/components/bootstrap/card/BootstrapCardConfig.js
+++ src/components/bootstrap/card/BootstrapCardConfig.js
@@ -3,15 +3,11 @@ import './styles.scss';
 
 // scripts
 export default class {
-  config = {
-    // update vue configuration here
-    // "name" and "template" should not be added here (as they are dynamically extracted from HTML)
-    data() {
-      return {
-        ReebokButtonConfig:{
-          buttonClicked() {
-            alert('Hi...I am a bootstrap card!!!')
-          }
+  data = () {
+    return {
+      ReebokButtonConfig:{
+        buttonClicked() {
+          alert('Hi...I am a bootstrap card!!!')
         }
       }
     }
