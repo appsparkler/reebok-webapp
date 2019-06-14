a9cd5b00fd55454f522b6b626aa952ec66eb5dc6
Akash <subs@appsparkler.com>
2019-06-14 08:58:15 +0530

    WIP

--- src/components/bootstrap/button/BootstrapButtonConfig.js
+++ src/components/bootstrap/button/BootstrapButtonConfig.js
@@ -4,9 +4,6 @@ import './styles.scss';
 // scripts
 export default class {
   props = ['componentApi']
-  mounted = function() {
-    alert('button is mounted...')
-  }
   methods = {
     buttonClicked() {
       if(this.componentApi && this.componentApi.buttonClicked)
